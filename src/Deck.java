93a7a73074fb9af290a0f8902711fefb80f8d279
John Floyd <john.floyd@menloschool.org>
2024-11-19 10:46:32 -0800

    Worked on deck classs

--- src/Deck.java
+++ src/Deck.java
@@ -1,4 +1,17 @@
 import java.util.ArrayList;
 public class Deck {
-    private ArrayList<Card> deck;
+    private ArrayList<Card> cards; // list if cards
+    private int cardsLeft;  // int to show how manny cards are left 
+
+    //construct a deck of cards using a nested loop
+    public Deck(String[] ranks, String[] suits, int[] values) {
+        cards = new ArrayList<>();
+        for (String suit : suits) {
+            for (int i = 0; i < ranks.length; i++) {
+                cards.add(new Card(ranks[i], suit, values[i]));
+            }
+        }
+        cardsLeft = cards.size();
+    }
+
 }
